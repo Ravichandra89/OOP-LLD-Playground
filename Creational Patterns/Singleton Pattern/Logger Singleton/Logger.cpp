ea8373aac98e75f297d5ca68a16e022ea94ac8c7
Ravi chandra <githubravi01@gmail.com>
2025-04-16 14:07:01 +0530

    Multithread problem

--- Creational Patterns/Singleton Pattern/Logger Singleton/Logger.cpp
+++ Creational Patterns/Singleton Pattern/Logger Singleton/Logger.cpp
@@ -5,11 +5,11 @@
 using namespace std;
 
 int Logger::count = 0;
-Logger* Logger::loggerInstance = new Logger();
+Logger* Logger::loggerInstance = nullptr;
 
 Logger::Logger() {
     count++;
-    cout << "Logger Instantiated : And Instance Count" << count << endl;
+    cout << "Logger Instantiated : And Instance Count " << count << endl;
 }
 
 void Logger::logs(const string & message) {
--- Creational Patterns/Singleton Pattern/Logger Singleton/User.cpp
+++ Creational Patterns/Singleton Pattern/Logger Singleton/User.cpp
@@ -1,17 +1,26 @@
 #include "Logger.hpp"
 #include <iostream>
+#include <thread>
+
 using namespace std;
 
-int main() {
+void user1Logs() {
     Logger * log = Logger::getLogger();
     log->logs("This is message from User 1");
+}
 
+void user2Logs() {
     Logger * logs2 = Logger::getLogger();
     logs2->logs("This is message from User 2");
+}
 
-    // Clean up
-    // delete log;
-    // delete logs2;
+int main() {
+    thread t1(user1Logs);
+    thread t2(user2Logs);
 
+    t1.join();
+    t2.join();
+
+    
     return 0;
 }
