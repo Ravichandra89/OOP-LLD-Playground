b41c996862956ed06fc9d7c88dc05b2bfbfdc76b
Ravi chandra <githubravi01@gmail.com>
2025-04-30 14:41:53 +0530

    Director Logic for Trip creation

---
+++ Creational Patterns/Builder Patter/Uber Builder Pattern/director/Director.cpp
@@ -0,0 +1,17 @@
+#include "Director.hpp"
+#include <iostream>
+#include "../builder/ITripBuilder.hpp"
+
+using namespace std;
+
+TripDirector::TripDirector(ITripBuilder *builder) : builder_(builder) {};
+
+void TripDirector::ConstructAirportTrip()
+{
+    builder_->setPickup("Hotel Taj Palce");
+    builder_->setDrop("International Airport");
+    builder_->setCarType("Standard");
+    builder_->setPayment("Card");
+    builder_->enableChildSeat();
+    builder_->addRoutePref("No Highways");
+};
---
+++ Creational Patterns/Builder Patter/Uber Builder Pattern/director/Director.hpp
@@ -0,0 +1,14 @@
+#ifndef Director_hpp
+#define Director_hpp
+
+#include "../builder/ITripBuilder.hpp"
+
+class TripDirector {
+    ITripBuilder* builder_;
+    public: 
+        // Constructor
+        TripDirector(ITripBuilder* builder);
+        void ConstructAirportTrip();
+};
+
+#endif
