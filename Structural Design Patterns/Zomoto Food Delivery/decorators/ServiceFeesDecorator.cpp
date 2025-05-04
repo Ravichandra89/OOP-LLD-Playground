fc5d2bf0ad64e2c888d115e69d93c7e3f0e5239a
Ravi chandra <githubravi01@gmail.com>
2025-05-04 22:48:36 +0530

    ServiceFees Decorator for on Food Delivery

---
+++ Structural Design Patterns/Zomoto Food Delivery/decorators/ServiceFeesDecorator.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "ServiceFeesDecorator.hpp"
+
+using namespace std;
+
+// Constructor
+ServiceFeesDecorator::ServiceFeesDecorator(IOrderCost * order, double cost) : OrderCostDecorator(order), serviceCharges(cost){};
+
+double ServiceFeesDecorator::calculateCost() const {
+    return order->calculateCost() + serviceCharges;
+}
