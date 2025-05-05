e9e5c7e89a1a15d25273e34fb09decdd1398d800
Ravi chandra <134200599+Ravichandra89@users.noreply.github.com>
2025-05-05 10:33:53 +0530

    Delete Structural Design Patterns/Zomoto Food Delivery directory

--- Structural Design Patterns/Zomoto Food Delivery/README.md
+++
@@ -1,1 +0,0 @@
-<img width="1470" alt="Screenshot 2025-05-05 at 10 16 49 AM" src="https://github.com/user-attachments/assets/91aa1e30-75a9-42d5-97c0-3adf249040ba" />
--- Structural Design Patterns/Zomoto Food Delivery/decorators/OrderCostDecorator.cpp
+++
@@ -1,9 +0,0 @@
-#include "OrderCostDecorator.hpp"
-#include <iostream>
-
-// Constructor 
-OrderCostDecorator::OrderCostDecorator(IOrderCost * order) : order(order){};
-
-OrderCostDecorator::~OrderCostDecorator() {
-    delete order;
-};
