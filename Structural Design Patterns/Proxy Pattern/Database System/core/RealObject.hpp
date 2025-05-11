1a319c6880b9a971869b65fd3a8627c2c25e66c2
Ravi chandra <githubravi01@gmail.com>
2025-05-11 08:56:39 +0530

    Updated RealObject Header File

--- Structural Design Patterns/Proxy Pattern/Database System/core/RealObject.hpp
+++ Structural Design Patterns/Proxy Pattern/Database System/core/RealObject.hpp
@@ -1,18 +1,23 @@
+/**
+ * Real Object Header File
+ */
+
 #ifndef REAL_OBJECT_HPP
 #define REAL_OBJECT_HPP
 
-// Database Interface
 #include "IDatabase.hpp"
-
-#include <string>
 #include <unordered_map>
 
+using namespace std;
+
 class RealObject : public IDatabase {
+        // <key, value> Databae
         unordered_map<string, string> store;
     public: 
         void create(const string & key, const string & value) override;
         string get(const string & key) override;
         void remove(const string & key) override;
 };
+
 
 #endif
