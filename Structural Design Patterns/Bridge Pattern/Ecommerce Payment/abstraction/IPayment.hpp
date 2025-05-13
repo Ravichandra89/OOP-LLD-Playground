ea95451b09789c32e925ddb939c8b938e533adea
Ravi chandra <githubravi01@gmail.com>
2025-05-13 11:39:25 +0530

    Abstraction Payment Interface

--- Structural Design Patterns/Bridge Pattern/Ecommerce Payment/abstraction/IPayment.hpp
+++ Structural Design Patterns/Bridge Pattern/Ecommerce Payment/abstraction/IPayment.hpp
@@ -6,13 +6,33 @@
 #define AMAZON_BRIDGE_PATTERN_ABSTRACTION_IPAYMENT_HPP
 
 #include <string>
+// Include the PaymentGateway header file
+#include "PaymentGateway.hpp"
 
+using namespace std;
 
-namespace abstraction {
+namespace abstraction
+{
 
-    class IPayment {
-        public: 
-        
+    class IPayment
+    {
+    protected:
+        unique_ptr<PaymentGateway> gateway_;
+
+    public:
+        virtual ~IPayment() = default;
+        explicit IPayment(unique_ptr<PaymentGateway> gateway);
+
+        // Non Copyable
+        IPayment(const IPayment &) = delete;
+        IPayment &operator=(const IPayment &) = delete;
+
+        // Non Movable
+        IPayment(IPayment &&) noexcept = default;
+        IPayment &operator=(IPayment &&) noexcept = default;
+
+        // Payment Pay method
+        virtual void pay(double amount) = 0;
     };
 }
 
