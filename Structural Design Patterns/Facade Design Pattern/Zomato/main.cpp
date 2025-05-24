33313c5152e44ca7d83e9311ecbedb793c454345
Ravi chandra <githubravi01@gmail.com>
2025-05-24 16:58:30 +0530

    Simplified Client code

---
+++ Structural Design Patterns/Facade Design Pattern/Zomato/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "ZomatoFacade.hpp"
+
+int main() {
+    ZomatoFacade order;
+    order.ZomatoPlaceOrder("Pizza Margherita", 2, 500, "CreditCard");
+    cout << "\n After Delivery " << endl;
+    order.ZomatoCompeleteOrder(1, "Delicious pizza and fast delivery!");
+
+    return 0;
+}
