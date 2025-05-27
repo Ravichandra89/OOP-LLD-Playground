dab6cc46085cbd6b278072bfd556a5633c6b1d86
Ravi chandra <githubravi01@gmail.com>
2025-05-27 14:16:41 +0530

    Ready State Code

--- Behavioral Design Pattern/State Pattern/Vending Machine/states/ReadyState.hpp
+++ Behavioral Design Pattern/State Pattern/Vending Machine/states/ReadyState.hpp
@@ -1,6 +1,12 @@
 #ifndef VENDING_MACHINE_READY_STATE_HPP
 #define VENDING_MACHINE_READY_STATE_HPP
 
+#include "StateInterface.hpp"
+
+class ReadyState : public StateInterface {
+    public: 
+        void handleRequest(VendingMachineContext& context) override;
+};
 
 
 #endif
