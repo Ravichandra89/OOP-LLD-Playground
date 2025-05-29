802c216b14b9786323272797c6faca18ccc84722
Ravi chandra <githubravi01@gmail.com>
2025-05-29 12:21:23 +0530

    Weather Station

--- Behavioral Design Pattern/Oberser Pattern/Weather Monitorin/Subject/weatherSubject.hpp
+++ Behavioral Design Pattern/Oberser Pattern/Weather Monitorin/Subject/weatherSubject.hpp
@@ -1,17 +1,35 @@
-#ifndef WEATHER_STATION_SUBJECT_HPP
-#define WEATHER_STATION_SUBJECT_HPP
+//  Subject for the Weather Monitoring System 
+#ifndef WEATHER_STATION_SYSTEM_SUBJECT_HPP
+#define WEATHER_STATION_SYSTEM_SUBJECT_HPP
 
+#include "Subject.hpp"
 #include <string>
+#include <vector> // For Observer List 
 using namespace std;
 
-class Observer;
+class WeatherStation : public Subject {
+        vector<Observer*> observers;
+        string weather;
 
-class Subject {
     public: 
-        virtual void addObserver(Observer* ob) = 0;
-        virtual void removeObserver(Observer* ob) = 0;
-        virtual void notifyObserver(Observer* ob) = 0;
-        virtual ~Subject() = default;
+        void addObserver(Observer* obs) override {
+            observers.push_back(obs);
+        }
+
+        void removeObserver(Observer* obs) override {
+            observers.erase(remove(observers.begin(), observers.end(), obs), observers.end());
+        }
+
+        void notifyObserver() override {
+            for (auto * it : observers) {
+                it->update(weather);
+            }
+        }
+
+        void setWeather(const string & changeWeather) {
+            weather = changeWeather;
+            notifyObserver();
+        }
 };
 
 #endif
