85f759af252aa6225fc79d28c5da74e3d5b1889e
Ravi chandra <githubravi01@gmail.com>
2025-06-02 21:37:20 +0530

    UPI Payment Concrete Strategy

--- Behavioral Design Pattern/Strategy Pattern/PaymentSystem/Strategy/UpiPayment.hpp
+++ Behavioral Design Pattern/Strategy Pattern/PaymentSystem/Strategy/UpiPayment.hpp
@@ -5,4 +5,14 @@
 #include <iostream>
 using namespace std;
 
+class UpiPayment : public IPaymentStrategy {
+    public : 
+        void pay(double amount) override {
+            cout << "📱 Initiating UPI transfer request to VPA..." << endl;
+            cout << "⏳ Awaiting UPI PIN verification..." << endl;
+            cout << "✅ ₹" << amount << "paied successfully via UPI" << endl << endl;
+        }
+};
+
+
 #endif
