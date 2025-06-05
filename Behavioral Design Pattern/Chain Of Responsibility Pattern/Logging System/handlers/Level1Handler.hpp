f138d04bb566eaea2dd6c18f71c9baf868f2debf
Ravi chandra <githubravi01@gmail.com>
2025-06-05 11:30:20 +0530

    Priority based logging handler

--- Behavioral Design Pattern/Chain Of Responsibility Pattern/Logging System/handlers/Level1Handler.hpp
+++ Behavioral Design Pattern/Chain Of Responsibility Pattern/Logging System/handlers/Level1Handler.hpp
@@ -3,7 +3,22 @@
 
 #include "SupportHandler.hpp"
 #include <memory>
+#include <iostream>
 
 using namespace std;
+
+class Level1Handler : public SupportHandler {
+    protected : 
+        bool processRequest(const Request& req) override {
+            // Check priority is 1 or not
+            if (req.priority == 1) {
+                cout << "[Level 1] Handling basic request:" << req.message << endl;
+                return true;
+            }
+
+            // If Priority differs
+            return false;
+        }
+};
 
 #endif
