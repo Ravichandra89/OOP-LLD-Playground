9d0dcba3f114f74a0db8736c658cb30f26e61690
Ravi chandra <githubravi01@gmail.com>
2025-06-14 11:10:09 +0530

    Parking Display Board - updatefreespots, show display methods

---
+++ Parking lot System/Include/ParkingDisplayBoard.hpp
@@ -0,0 +1,67 @@
+#ifndef PARKING_DISPLAY_BOARD_HPP
+#define PARKING_DISPLAY_BOARD_HPP
+
+#include "ParkingSpot.hpp"
+#include "Enums.hpp"
+
+#include <string>
+#include <iostream>
+#include <map>
+using namespace std;
+
+class ParkingDisplayBoard {
+        string boardId;
+        map<ParkingSpotType, int> freeSpotsCount;
+    
+    public : 
+        // Constructor
+        ParkingDisplayBoard(const string & id) : boardId(id){};
+
+        // Update the free spots count for each type of parking spot;
+        void updateFreeSpotsCount(const map<ParkingSpotType, int> & freeSpots) {
+            freeSpotsCount = freeSpots;
+            show();
+        }
+
+        // show the display board
+        void show() const {
+            cout << "[Display Board Id: " << boardId << " Free spots counts : ]" << endl;
+            
+            // Iterate through the map
+            for (const auto & it : freeSpotsCount) {
+                string spotType;
+                
+                switch(it.first) {
+                    case ParkingSpotType::COMPACT: 
+                        spotType = "COMPACT";
+                        break;
+                    
+                    case ParkingSpotType::LARGE:
+                        spotType = "LARGE";
+                        break;
+                    
+                    case ParkingSpotType::HANDICAPPED:
+                        spotType = "HANDICAPPED";
+                        break;
+                    
+                    case ParkingSpotType::MOTORCYCLE:
+                        spotType = "MOTORCYCLE";
+                        break;
+
+                    case ParkingSpotType::ELECTRIC:
+                        spotType = "ELECTRIC";
+                        break;
+
+                    default: 
+                        spotType = "UNKOWN";
+                        break;
+                }
+                // showing the type free spots count
+                cout << spotType << " : " << it.second << endl;
+            }
+
+        }
+
+};
+
+#endif
