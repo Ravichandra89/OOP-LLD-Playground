13623f244707dba73be7b50ab35188a42a45ea22
Ravi chandra <githubravi01@gmail.com>
2025-06-17 13:37:32 +0530

    NoCommand Class

---
+++ Behavioral Design Pattern/Command Design Pattern/Remote Controle Home Decives/commands/Nocommand.hpp
@@ -0,0 +1,13 @@
+#ifndef NO_COMMAND_HPP
+#define NO_COMMAND_HPP
+
+#include "Command.hpp"
+
+class NoCommand : public Command {
+    public : 
+        void execute() override {
+
+        }
+};
+
+#endif
