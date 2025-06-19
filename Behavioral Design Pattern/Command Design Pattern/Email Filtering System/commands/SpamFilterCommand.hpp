1d88690adc890804a37455384fcaf9fc688799a7
Ravi chandra <githubravi01@gmail.com>
2025-06-19 11:43:34 +0530

    Spam Concrete Command

--- Behavioral Design Pattern/Command Design Pattern/Email Filtering System/commands/SpamFilterCommand.hpp
+++ Behavioral Design Pattern/Command Design Pattern/Email Filtering System/commands/SpamFilterCommand.hpp
@@ -1,11 +1,22 @@
 #ifndef SPAM_FILTER_COMMAND_HPP
 #define SPAM_FILTER_COMMAND_HPP
 
-#include "Icommand.hpp"
+#include "ICommand.hpp"
 #include <iostream>
 using namespace std;
 
-
+class SpamFilterCommand : public ICommand {
+    public : 
+        void execute(Email & email) override {
+            // Check the email body first
+            if (email.body.find("win money") != string::npos || email.body.find("lottry") != string::npos) {
+                email.is_Spam = true;
+                cout << "Email is Spam!" << endl;
+            } else {
+                cout << "Spam Filter is Passed" << endl;
+            }
+        } 
+};
 
 
 #endif
