26811bc54225224c6fa1fd854367049d54a1e3e1
Ravi chandra <githubravi01@gmail.com>
2025-06-27 14:34:40 +0530

    ArrayList Concrete Aggregator

--- Behavioral Design Pattern/Iterator Design Pattern/Java Collection Iterator/include/ArrayList.hpp
+++ Behavioral Design Pattern/Iterator Design Pattern/Java Collection Iterator/include/ArrayList.hpp
@@ -1,11 +1,29 @@
+/**
+ * Concrete Aggregator - Implementing the Collection Interface
+ */
+
 #ifndef ARRAY_LIST_HPP
 #define ARRAY_LIST_HPP
 
 #include "Iterable.hpp"
+#include "../iterator/arrayListIterator.hpp"
+
+#include <vector>
 using namespace std;
 
-class ArrayList : public Iterable {
-    
+class ArrayList : public IterableCollection {
+        vector<int> data_;
+
+    public : 
+        // add value to data vector
+        void add(int val) {
+            data_.push_back(val);
+        }
+
+        // Aggregate Object Creator
+        unique_ptr<Iterable> iterator() override {
+            return make_unique<ArrayListIterator>(data_);
+        }
 };
 
 #endif
