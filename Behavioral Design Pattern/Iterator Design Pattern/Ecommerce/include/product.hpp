42afd789143b92af755f4362e6f560457a5c9848
Ravi chandra <githubravi01@gmail.com>
2025-06-28 11:53:51 +0530

    Ecommerce Product Iterator Example

---
+++ Behavioral Design Pattern/Iterator Design Pattern/Ecommerce/include/product.hpp
@@ -0,0 +1,15 @@
+#ifndef PRODUCT_HPP
+#define PRODUCT_HPP
+
+// Product Structure - Data Fields
+
+#include<string>
+using namespace std;
+
+struct Product {
+    int id;
+    string name;
+    double price;
+};
+
+#endif
