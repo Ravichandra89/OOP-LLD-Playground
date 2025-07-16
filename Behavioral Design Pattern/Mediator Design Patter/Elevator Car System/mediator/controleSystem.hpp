d37307e9586110c0d3cc63d5a2cc9639ebf5e1d3
Ravi chandra <githubravi01@gmail.com>
2025-07-16 17:29:20 +0530

    Elevator Car System :: By Mediator Controller

---
+++ Behavioral Design Pattern/Mediator Design Patter/Elevator Car System/mediator/controleSystem.hpp
@@ -0,0 +1,42 @@
+/**
+ * Define the whole control system mediator interface.
+ */
+
+#ifndef MEDIATOR_CONTROLESYSTEM_HPP
+#define MEDIATOR_CONTROLESYSTEM_HPP
+
+#include <string>
+#include <vector>
+using namespace std;
+
+// Forward Declarations of Colleagues
+class CarPanel;
+class FloorPanel;
+class ElevatorCar;
+
+class ControleSystem
+{
+
+private:
+    // Private members can be added here if needed
+    vector<ElevatorCar *> cars;
+    vector<FloorPanel *> floors;
+    vector<CarPanel *> cars;
+
+    // internal methods
+    void dispatchCar(int floor, bool direction);
+    void handleDropOff(int carId, int floor);
+
+public:
+    virtual ~ControleSystem() = default;
+
+    virtual void registerCar(ElevatorCar *car) = 0;
+    virtual void registerFloorPanel(FloorPanel *panel) = 0;
+    virtual void registerCarPanel(CarPanel *panel) = 0;
+
+    // Pickup and drop off requests handlers
+    virtual void requestPickup(int carId, int floor) = 0;
+    virtual void requestDropOff(int carId, int floor) = 0;
+};
+
+#endif
